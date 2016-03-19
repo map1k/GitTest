986bb2475135e712b816e63d68e3000fae43181d
map1kmail@gmail.com <456789fisa>
2016-03-20 00:13:32 +0400

    Commit

--- src/main/java/com/gitTest/marat/TestClass.java
+++ src/main/java/com/gitTest/marat/TestClass.java
@@ -2,6 +2,9 @@ package com.gitTest.marat;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("Hello git");
+            System.out.println("Hello git");
+        System.out.println('w');
+        System.out.println('a');
+        System.out.println('t');
     }
 }
