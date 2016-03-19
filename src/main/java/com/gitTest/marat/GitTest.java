f46f645474a79c1369e11c331da5bdcbca27c761
map1kmail@gmail.com <456789fisa>
2016-03-20 00:23:21 +0400

    Commit 2

---
+++ src/main/java/com/gitTest/marat/GitTest.java
@@ -0,0 +1,10 @@
+package com.gitTest.marat;
+
+/**
+ * Created by Марат on 20.03.16.
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("!11");
+    }
+}
